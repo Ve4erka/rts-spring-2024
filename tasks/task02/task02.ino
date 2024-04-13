268a07fe9f3210896bb922557c9c4ea48ab1594a
Ve4erka <100806493+Ve4erka@users.noreply.github.com>
2024-04-13 21:39:49 +0400

    Update task02.ino

--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,93 +1,64 @@
-#include <TaskManagerIO.h>
- 
-const int led_pin = 3;
-const int led_pin2 = 5;
-const int led_pin3 = 6;
-const int led_pin4 = 9;
-const int led_pin5 = 10;
+#include <TaskManagerIO.h>;
+const int ledPin3 = 3;  
+const int ledPin5 = 5;  
+const int ledPin6 = 6;  
+const int ledPin9 = 9;  
+const int ledPin10 = 10;  
 
 
-int led_state = LOW; 
-int led_state2 = LOW;
-int led_state3 = LOW;
-int led_state4 = LOW;
-int led_state5 = LOW;
+const int interval3 = 10000;  
+const int interval5 = 1000;  
+const int interval6 = 500;  
+const int interval9 = 100;  
+const int interval10 = 50;
 
+int ledStatePin3 = LOW;
+int ledStatePin5 = LOW;
+int ledStatePin6 = LOW;
+int ledStatePin9 = LOW;
+int ledStatePin10 = LOW;
 
-unsigned long previous_millis = 0; 
-unsigned long previous_millis2 = 0; 
-unsigned long previous_millis3 = 0; 
-unsigned long previous_millis4 = 0; 
-unsigned long previous_millis5 = 0;  
-
-
-const long interval = 10000;
-const long interval2 = 1000;
-const long interval3 = 500;
-const long interval4 = 100;
-const long interval5 = 50;  
-
-void blink()
-{
-  if (led_state == LOW) {
-      led_state = HIGH;
-    } else {
-      led_state = LOW;
-    }
-  digitalWrite(led_pin, led_state);
-}
-void blink2()
-{
-  if (led_state2 == LOW) {
-      led_state2 = HIGH;
-    } else {
-      led_state2 = LOW;
-    }
-  digitalWrite(led_pin2, led_state2);
-}
-void blink3()
-{
-  if (led_state3 == LOW) {
-      led_state3 = HIGH;
-    } else {
-      led_state3 = LOW;
-    }
-  digitalWrite(led_pin3, led_state3);
-}
-void blink4()
-{
-  if (led_state4 == LOW) {
-      led_state4 = HIGH;
-    } else {
-      led_state4 = LOW;
-    }
-  digitalWrite(led_pin4, led_state4);
-}
-void blink5()
-{
-  if (led_state5 == LOW) {
-      led_state5 = HIGH;
-    } else {
-      led_state5 = LOW;
-    }
-  digitalWrite(led_pin5, led_state5);
+void toggleLed(int pin, int &ledStatePin) {
+  	if (ledStatePin == LOW)
+      {
+        ledStatePin =  HIGH;
+      }
+      else 
+      {
+        ledStatePin = LOW;
+      }
+      digitalWrite(pin, ledStatePin);
 }
 
 
 void setup() {
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin5, OUTPUT);
+  pinMode(ledPin6, OUTPUT);
+  pinMode(ledPin9, OUTPUT);
+  pinMode(ledPin10, OUTPUT);
 
-  pinMode(led_pin, OUTPUT);
-  pinMode(led_pin2, OUTPUT);
-  pinMode(led_pin3, OUTPUT);
-  pinMode(led_pin4, OUTPUT);
-  pinMode(led_pin5, OUTPUT);
-  
-  taskManager.schedule(repeatMicros(interval), blink);
-  taskManager.schedule(repeatMicros(interval2), blink2);
-  taskManager.schedule(repeatMicros(interval3), blink3);
-  taskManager.schedule(repeatMicros(interval4), blink4);
-  taskManager.schedule(repeatMicros(interval5), blink5);
+taskManager.scheduleFixedRate(interval3, [] {
+    toggleLed(ledPin3, ledStatePin3);
+	}, TIME_MICROS);
+
+taskManager.scheduleFixedRate(interval5, [] {
+		toggleLed(ledPin5, ledStatePin5);
+	}, TIME_MICROS);
+
+taskManager.scheduleFixedRate(interval6, [] {
+		toggleLed(ledPin6, ledStatePin6);
+	}, TIME_MICROS);
+
+taskManager.scheduleFixedRate(interval9, [] {
+		toggleLed(ledPin9, ledStatePin9);
+	}, TIME_MICROS);
+
+taskManager.scheduleFixedRate(interval10, [] {
+		toggleLed(ledPin10, ledStatePin10);
+	}, TIME_MICROS);
 }
+
 
 void loop() {
   taskManager.runLoop();
